43eb32dca33adace61947d81d647f2da55685575
Jian Liao <jianliao@adobe.com>
2024-11-02 17:24:11 -0700

    Removed basePath

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/blog/next.svg"
+          src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/blog/vercel.svg"
+              src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +54,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/blog/file.svg"
+            src="/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +68,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/blog/window.svg"
+            src="/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/blog/globe.svg"
+            src="/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
