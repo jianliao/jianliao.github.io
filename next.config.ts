e87135647837331412cf1a478667329d3832ba86
Jian Liao <jianliao@adobe.com>
2024-11-02 18:44:45 -0700

    Integrated @next/mdx package and mdx file type

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,25 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+import slug from 'rehype-slug';
+import tableOfContents from 'remark-toc';
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [slug],
+    remarkPlugins: [tableOfContents],
+  },
+})
+
+export default withMDX(nextConfig);
