e3b1cef4b3e20b7336510f48a8a6860ebc7f770a
Jian Liao <jianliao@adobe.com>
2024-11-05 18:48:10 -0800

    Switched to the google variable font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,20 @@ import "./globals.css";
 import "katex/dist/katex.min.css";
 
 import type { Metadata } from "next";
-import Script from "next/script";
+import { Inter } from 'next/font/google';
 import { ThemeProvider } from "next-themes";
 
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import LocalFont from "@/lib/local-font";
 import { HomePage } from "@/data/meta-data";
 
 export const metadata: Metadata = HomePage.metadata;
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 
 export default function RootLayout({
   children,
@@ -19,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={`flex flex-col h-screen ${LocalFont.className}`}>
+      <body className={`flex flex-col h-screen ${inter.className}`}>
         <ThemeProvider enableSystem={true} attribute="class">
           <Header />
           <main className="max-w-4xl container text-black dark:text-white mx-auto px-6 py-6 flex-1 leading-6">
@@ -27,13 +32,6 @@ export default function RootLayout({
           </main>
           <Footer />
         </ThemeProvider>
-        {/* {process.env.NODE_ENV === "production" && (
-          <Script
-            async
-            src={process.env.UMAMI_SCRIPT_URL}
-            data-website-id={process.env.UMAMI_DATA_WEBSITE_ID}
-          />
-        )} */}
       </body>
     </html>
   );
