139a388f64e54719bc01e012e8970e21da6643dc
Jian Liao <jianliao@adobe.com>
2024-11-26 19:25:40 -0800

    Added thumbnail to blog list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -16,11 +16,20 @@ export default function Blog() {
               key={post.slug}
               className="flex flex-col w-full dark:hover:bg-slate-900 border hover:bg-slate-50 rounded p-4 mb-4 dark:border-slate-600 dark:bg-black hover:shadow-sm dark:hover:shadow-sm"
             >
-              <span className="font-bold">{post.title}</span>
-              <div className="flex justify-between">
-                <time className="text-sm text-slate-400 mt-2">
-                  {post.publishedAt}
-                </time>
+              <div className="flex flex-row justify-between">
+                {/* Stack title and time vertically */}
+                <div className="flex flex-col">
+                  <span className="font-bold">{post.title}</span>
+                  <time className="text-sm text-slate-400 mt-2">
+                    {post.publishedAt}
+                  </time>
+                </div>
+                {/* Thumbnail on the far right */}
+                <img
+                  src={post.thumbnail}
+                  alt={post.title}
+                  className="w-16 h-16 object-cover ml-4 rounded"
+                />
               </div>
             </li>
           </Link>
