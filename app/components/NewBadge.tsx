13c93d5dfda2579f3786776ef5a25621dd7ce06a
Jian Liao <jianliao@adobe.com>
2025-10-18 18:46:24 -0700

    Add newbadge component to latest article

---
+++ app/components/NewBadge.tsx
@@ -0,0 +1,8 @@
+export default function NewBadge() {
+  return (
+    <span className="inline-block ml-2 px-2 py-1 text-xs font-semibold text-white bg-gradient-to-r from-blue-500 to-purple-600 rounded-full">
+      NEW
+    </span>
+  );
+}
+
